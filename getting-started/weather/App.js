4827d886266cce069f94d28c2d6554d07a1bd572
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2020-02-29 10:15:50 -0800

    start ImageBackgroundView (51)

--- getting-started/weather/App.js
+++ getting-started/weather/App.js
@@ -1,22 +1,36 @@
 import React from "react";
 import {
   StyleSheet,
+  View,
+  ImageBackground,
   Text,
   Platform,
-  TextInput,
   KeyboardAvoidingView
 } from "react-native";
 
+import getImageForWeather from "./utils/getImageForWeather";
 import SearchInput from "./components/SearchInput";
 
 export default class App extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="height">
-        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-        <SearchInput placeholder="Search any city" />
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              San Francisco
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -25,13 +39,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#34495E"
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
+    paddingHorizontal: 20
   },
   textStyle: {
     textAlign: "center",
-    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto"
+    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto",
+    color: "white"
   },
   largeText: {
     fontSize: 44
